b0b620c1fae84b4ecb09af934fb2a724f756ebb7
sauloapp <sauloaugusto@gmail.com>
2024-06-28 14:19:03 -0300

    Changes for gencode adapters work for dmel+hsa
    Former-commit-id: 0fab78712a98c1db1a4f181d7580cfe415f18233

--- biocypher_metta/adapters/uniprot_adapter.py
+++ biocypher_metta/adapters/uniprot_adapter.py
@@ -16,7 +16,7 @@ class UniprotAdapter(Adapter):
     ALLOWED_TYPES = ['translates to', 'translation of']
     ALLOWED_LABELS = ['translates_to', 'translation_of']
 
-    def __init__(self, filepath, type, label,
+    def __init__(self, dmel_filepath, hsa_filepath, type, label,
                  write_properties, add_provenance):
         if type not in UniprotAdapter.ALLOWED_TYPES:
             raise ValueError('Invalid type. Allowed values: ' +
@@ -24,7 +24,8 @@ class UniprotAdapter(Adapter):
         if label not in UniprotAdapter.ALLOWED_LABELS:
             raise ValueError('Invalid label. Allowed values: ' +
                              ', '.join(UniprotAdapter.ALLOWED_LABELS))
-        self.filepath = filepath
+        self.dmel_filepath = dmel_filepath
+        self.hsa_filepath = hsa_filepath
         self.dataset = label
         self.type = type
         self.label = label
@@ -34,13 +35,12 @@ class UniprotAdapter(Adapter):
         super(UniprotAdapter, self).__init__(write_properties, add_provenance)
 
     def get_edges(self):
-        with gzip.open(self.filepath, 'rt') as input_file:
+        with gzip.open(self.dmel_filepath, 'rt') as input_file:
             records = SeqIO.parse(input_file, 'swiss')
             for record in records:
                 if self.type == 'translates to':
                     dbxrefs = record.dbxrefs
                     for item in dbxrefs:
-                        #if item.startswith('Ensembl') and 'ENST' in item:
                         if item.startswith('EnsemblMetazoa') and 'FBtr' in item:
                             try:
                                 ensg_id = item.split(':')[-1].split('.')[0]
@@ -62,6 +62,50 @@ class UniprotAdapter(Adapter):
                     for item in dbxrefs:
                         #if item.startswith('Ensembl') and 'ENST' in item:
                         if item.startswith('EnsemblMetazoa') and 'FBtr' in item:
+                            try:
+                                ensg_id = item.split(':')[-1].split('.')[0]
+                                _id = ensg_id + '_' + record.id
+                                _target = ensg_id
+                                _source = record.id
+                                _props = {}
+                                if self.write_properties and self.add_provenance:
+                                    _props['source'] = self.source
+                                    _props['source_url'] = self.source_url
+                                yield  _source, _target, self.label, _props
+
+                            except:
+                                logger.info(
+                                    f'fail to process for edge translation of: {record.id}')
+                                pass
+
+        with gzip.open(self.hsa_filepath, 'rt') as input_file:
+            records = SeqIO.parse(input_file, 'swiss')
+            for record in records:
+                if self.type == 'translates to':
+                    dbxrefs = record.dbxrefs
+                    for item in dbxrefs:
+                        if item.startswith('Ensembl') and 'ENST' in item:
+                        #if item.startswith('EnsemblMetazoa') and 'FBtr' in item:
+                            try:
+                                ensg_id = item.split(':')[-1].split('.')[0]
+                                _id = record.id + '_' + ensg_id
+                                _source = ensg_id
+                                _target = record.id
+                                _props = {}
+                                if self.write_properties and self.add_provenance:
+                                    _props['source'] = self.source
+                                    _props['source_url'] = self.source_url
+                                yield _source, _target, self.label, _props
+
+                            except:
+                                logger.info(
+                                    f'fail to process for edge translates to: {record.id}')
+                                pass
+                elif self.type == 'translation of':
+                    dbxrefs = record.dbxrefs
+                    for item in dbxrefs:
+                        if item.startswith('Ensembl') and 'ENST' in item:
+                        #if item.startswith('EnsemblMetazoa') and 'FBtr' in item:
                             try:
                                 ensg_id = item.split(':')[-1].split('.')[0]
                                 _id = ensg_id + '_' + record.id
