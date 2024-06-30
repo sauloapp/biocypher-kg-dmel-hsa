6f3744bed7c93c12e69f9643e3fac30ca5f40ffe
sauloapp <sauloaugusto@gmail.com>
2024-06-30 12:06:39 -0700

    renaming of dmel create dicts. Updating of adapters_config.yaml for STRING
    Former-commit-id: 988e11e924b5fbcced287a0452bbcfa18976d287

--- scripts/create_dmel_ensembl_to_uniprot_id.py
+++ scripts/create_dmel_ensembl_to_uniprot_id.py
@@ -4,23 +4,22 @@ from Bio import SeqIO
 import pickle
 
 def create_ensembl_to_uniprot_dict(input_uniprot, ensembl_to_uniprot_output):
-    ensembl_uniprot_ds = {}
+    ensembl_uniprot_ids = {}
     with gzip.open(input_uniprot, 'rt') as input_file:
-        records = SeqIO.parse(input_uniprot, 'swiss')
+        records = SeqIO.parse(input_file, 'swiss')
         for record in records:
             dbxrefs = record.dbxrefs
-
             for item in dbxrefs:
-                if item.startswith('EnsemblMetazoa') and 'FBpp' in item:
+                if item.startswith('STRING'):            
                     try:
-                        ensembl_id = item.split(':')[-1].split('.')[0]
-                        uniprot_id = record.id
+                        ensembl_id = item.split(':')[-1].split('.')[1]
+                        uniprot_id = record.name.replace("_DROME", "")
                         if ensembl_id:
-                            ensembl_uniprot_ds[ensembl_id] = uniprot_id
+                            ensembl_uniprot_ids[ensembl_id] = uniprot_id
                     except:
-                        print(f'fail to process for edge translates to: {record.id}')
+                        print(f'fail to process record: {record.name}')
     with open(ensembl_to_uniprot_output, 'wb') as pickle_file:
-        pickle.dump(gene_dbxrefs, pickle_file)
+        pickle.dump(ensembl_uniprot_ids, pickle_file)
 
 
 dmel_uniprot_input_file = '/mnt/hdd_2/saulo/snet/rejuve.bio/das/shared_rep/data/input/full/uniprot/uniprot_sprot_invertebrates_DMEL.dat.gz'
