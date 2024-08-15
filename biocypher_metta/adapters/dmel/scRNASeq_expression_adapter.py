459af05cd4020edbe9a9f395ea03a3e25d117192
sauloapp <sauloaugusto@gmail.com>
2024-08-14 22:21:34 -0300

    3 expression numerical data adapters with dmel data

--- biocypher_metta/adapters/dmel/scRNASeq_expression_adapter.py
+++ biocypher_metta/adapters/dmel/scRNASeq_expression_adapter.py
@@ -1,30 +1,48 @@
 '''
 
-# https://wiki.flybase.org/wiki/FlyBase:Downloads_Overview#Genetic_interaction_table_.28gene_genetic_interactions_.2A.tsv.29
-gene genetic association:
+# The unique ID for a library at Flybase is its FBlc#
+scRNASeq library:
+  is_a: entity
+  represented_as: node
+  inherit_properties: false
+  input_label: scrnaseq_library
   description: >-
-    An association between a gene and another gene, i.e., a gene-level genetic interactions in FlyBase. This data is computed from the allele-level genetic interaction data captured by FlyBase curators.
-  is_a: regulatory association
-  inherit_properties: true
-  represented_as: edge
-  input_label: gene_genetic
-  source: gene
-  target: gene
+      An experiment that is a set of genes and their expression values.
   properties:
-    type: str                   # suppresses (“suppressible”) / enhances (“enhanceable”)
-    reference: str              # FBrf#
-    taxon_id: int                        # 7227 for dmel / 9606 for hsa
+    name: str
+    experiment_info: str[]
+    tissue_info: str[]
+    cell_type: str
 
-##Starting_gene(s)_symbol	Starting_gene(s)_FBgn	Interacting_gene(s)_symbol	Interacting_gene(s)_FBgn	Interaction_type	Publication_FBrf
-Pfdn2	FBgn0010741	aPKC	FBgn0261854	suppressible	FBrf0231608
-Col4a1	FBgn0000299	vkg	FBgn0016075	suppressible	FBrf0217234
-dx	FBgn0000524	Su(dx)	FBgn0003557	suppressible	FBrf0068437
-dsh	FBgn0000499	msn	FBgn0010909	suppressible	FBrf0111453
-Rac1	FBgn0010333	upd1	FBgn0004956	suppressible	FBrf0191759
-aos	FBgn0004569	pix	FBgn0086706	enhanceable	FBrf0190712
-z	FBgn0004050	Scm	FBgn0003334	suppressible	FBrf0104734
+FB  data:
+https://wiki.flybase.org/wiki/FlyBase:Downloads_Overview#High-Throughput_Gene_Expression_.28high-throughput_gene_expression_fb_.2A.tsv.gz.29
+https://wiki.flybase.org/wiki/FlyBase:Downloads_Overview#Single_Cell_RNA-Seq_Gene_Expression_.28scRNA-Seq_gene_expression_fb_.2A.tsv.gz.29
+
+FB scRNASeq table:
+#Pub_ID	Pub_miniref	Clustering_Analysis_ID	Clustering_Analysis_Name	Source_Tissue_Sex	Source_Tissue_Stage	Source_Tissue_Anatomy	Cluster_ID	Cluster_Name	Cluster_Cell_Type_ID	Cluster_Cell_Type_Name	Gene_ID	Gene_Symbol	Mean_Expression	Spread
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0031081	Nep3	1022.4949	0.00016897600540723216
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0031088	CG15322	269.05170000000004	0.0005069280162216965
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0053217	CG33217	439.74384371428573	0.026022304832713755
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0052350	Vps11	585.499525895105	0.024163568773234202
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0024733	RpL10	3497.867660248448	0.9793849273403177
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0040372	G9a	602.1811133469388	0.05795876985468063
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0000316	cin	582.4078043088889	0.03801960121662724
+FBrf0245988	Cattenoz et al., 2020, EMBO J. 39(12): e104486	FBlc0003731	scRNAseq_2020_Cattenoz_NI_seq_clustering		larval stage	embryonic/larval hemolymph	FBlc0003732	scRNAseq_2020_Cattenoz_NI_seq_clustering_plasmatocytes	FBbt:00001685	embryonic/larval plasmatocyte	FBgn0024989	CG3777	354.646665	0.0003379520108144643
+
+
+FB high-throughput table
+#High_Throughput_Expression_Section	Dataset_ID	Dataset_Name	Sample_ID	Sample_Name	Gene_ID	Gene_Symbol	Expression_Unit	Expression_Value
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003619	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Brain	FBgn0000003	7SLRNA:CR32864	FPKM	202396
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003619	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Brain	FBgn0289744	EndoG	FPKM	12
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003619	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Brain	FBgn0289745	Est17	FPKM	0
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003619	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Brain	FBgn0289868	Or67c	FPKM	0
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003620	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Crop	FBgn0000003	7SLRNA:CR32864	FPKM	240220
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003620	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Crop	FBgn0000008	a	FPKM	2
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003620	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Crop	FBgn0000014	abd-A	FPKM	0
+FlyAtlas2 Anatomy RNA-Seq	FBlc0003498	FlyAtlas2	FBlc0003620	RNA-Seq_Profile_FlyAtlas2_Adult_Female_Crop	FBgn0000015	Abd-B	FPKM	0
 
 '''
+
 
 from biocypher_metta.adapters.dmel.flybase_tsv_reader import FlybasePrecomputedTable
 from biocypher_metta.adapters import Adapter
@@ -33,57 +51,45 @@ from biocypher._logger import logger
 
 class ScRNASeqExpressionAdapter(Adapter):
 
-    def __init__(self, write_properties, add_provenance, dmel_data_filepath):
-        self.dmel_data_filepath = dmel_data_filepath
-        self.label = 'gene_genetic_association'
+    def __init__(self, write_properties, add_provenance, dmel_data_filepaths: list[str]):
+        self.dmel_data_filepaths = dmel_data_filepaths
+        self.label = 'scrnaseq_library'
         self.source = 'FLYBASE'
         self.source_url = 'https://flybase.org/'
 
         super(ScRNASeqExpressionAdapter, self).__init__(write_properties, add_provenance)
 
 
-    def get_edges(self):
-        expression_table= FlybasePrecomputedTable(self.dmel_data_filepath)
-        self.version = expression_table.extract_date_string(self.dmel_data_filepath)
-        # header:
-        #Starting_gene(s)_symbol	Starting_gene(s)_FBgn	Interacting_gene(s)_symbol	Interacting_gene(s)_FBgn	Interaction_type	Publication_FBrf
-        rows = expression_table.get_rows()
-        for row in rows:
-            if "Starting_gene(s)_FBgn" in row[1]:     # to skip header (columns' names)
-                continue
-            props = {}
-            #source_ids = row[1].split('|')
-            source = [id for id in row[1].split('|')]
-            target = [id for id in row[3].split('|')]
-            # most frequent case:
-            if len(source) == 1 and len(target) == 1:
-                source = source[0]
-                target = target[0]
-                props['type'] = row[4]
-                props['reference'] = row[5]
-                props['taxon_id'] = 7227
-                yield source, target, self.label, props
-            #
-            elif len(source) > 1 and len(target) == 1:
-                target = target[0]
-                for source_id in source:
-                    props['type'] = row[4]
-                    props['reference'] = row[5]
-                    props['taxon_id'] = 7227
-                    yield source_id, target, self.label, props
-            #
-            elif len(source) == 1 and len(target) > 1:
-                source = source[0]
-                for target_id in target:
-                    props['type'] = row[4]
-                    props['reference'] = row[5]
-                    props['taxon_id'] = 7227
-                    yield source, target_id, self.label, props
-            #
-            elif len(source) > 1 and len(target) > 1:
-                for source_id in source:
-                    for target_id in target:
-                        props['type'] = row[4]
-                        props['reference'] = row[5]
-                        props['taxon_id'] = 7227
-                        yield source_id, target_id, self.label, props
+    def get_nodes(self):
+        for dmel_data_filepath in self.dmel_data_filepaths:
+            if "scRNA-Seq" in dmel_data_filepath:
+                expression_table = FlybasePrecomputedTable(dmel_data_filepath)
+                self.version = expression_table.extract_date_string(dmel_data_filepath)
+                # header:
+                # Pub_ID	Pub_miniref	Clustering_Analysis_ID	Clustering_Analysis_Name	Source_Tissue_Sex	Source_Tissue_Stage
+                # Source_Tissue_Anatomy	Cluster_ID	Cluster_Name	Cluster_Cell_Type_ID	Cluster_Cell_Type_Name	Gene_ID	Gene_Symbol	Mean_Expression	Spread
+                rows = expression_table.get_rows()
+                for row in rows:
+                    library_id = row[7]     # Cluster_ID
+                    props = {
+                        "name": row[8],         # Cluster_Name
+                        "experiment_info": [row[2], row[3]],        # Clustering_Analysis_ID, Clustering_Analysis_Name
+                        "tissue_info": [row[i] for i in [4,5,6] if row[i] != ''],    # Source_Tissue_Sex	Source_Tissue_Stage, Source_Tissue_Anatomy
+                        "cell_type_id": row[9],     # Cluster_Cell_Type_ID (Flybase ontology FBbt:#)
+                        "taxon_id": 7227
+                    }
+                    yield library_id, self.label, props
+            elif "high-throughput" in dmel_data_filepath:
+                expression_table = FlybasePrecomputedTable(dmel_data_filepath)
+                self.version = expression_table.extract_date_string(dmel_data_filepath)
+                # FB high-throughput table header
+                # High_Throughput_Expression_Section	Dataset_ID	Dataset_Name	Sample_ID	Sample_Name	Gene_ID	Gene_Symbol	Expression_Unit	Expression_Value
+                rows = expression_table.get_rows()
+                for row in rows:
+                    library_id = row[3]     # Sample_ID
+                    props = {
+                        "name": row[4],         # Sample_Name
+                        "experiment_info": [row[1], row[2]],        # Dataset_ID	Dataset_Name
+                        "taxon_id": 7227
+                    }
+                    yield library_id, self.label, props
